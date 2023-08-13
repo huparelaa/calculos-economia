2f0fc64e7d932d2eaa365ded7296d52c0063cf70
Hobar <huparelaa@eafit.edu.co>
2023-08-13 09:34:40 -0500

    ✨Added backend for conversion from effective tasaX to tasaY

--- calculos_api/equivalencias/urls.py
+++ calculos_api/equivalencias/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from equivalencias import views
 urlpatterns = [
     path("sum/", views.sum_numbers, name="sum-numbers"),
+    path("efectivaX_a_efectivaY/", views.efectivaX_a_efectivaY, name="efectivaX_a_efectivaY"),
+    path("getEquivalencias/", views.getEquivalencias, name="getEquivalencias"),
 ]
--- calculos_api/equivalencias/views.py
+++ calculos_api/equivalencias/views.py
@@ -1,6 +1,16 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 import json
+
+diccionario_equivalencias = {
+    "mensual": 12, 
+    "bimestral": 6, 
+    "trimestral": 4, 
+    "cuatrimestral": 3, 
+    "semestral": 2, 
+    "anual": 1
+    }
+
 @api_view(["POST"])
 def sum_numbers(request):
     try:
@@ -10,5 +20,28 @@ def sum_numbers(request):
         print(num1)
         result = num1 + num2
         return JsonResponse({"result": result})
+    except ValueError:
+        return JsonResponse({"error": "Invalid input. Please provide valid numbers."})
+
+@api_view(["GET"])
+def getEquivalencias(request):
+    try:
+        return JsonResponse(diccionario_equivalencias)
+    except ValueError:
+        return JsonResponse({"error": "Invalid input. Please provide valid numbers."})
+
+@api_view(["POST"])
+def efectivaX_a_efectivaY(request):
+    try:
+        data = json.loads(request.body)
+        i1 = float(data.get("i1", 0))
+        m1 = float(data.get("m1", 0))
+        m2 = float(data.get("m2", 0))
+
+        i2 = ((1+i1)**(m1/m2))-1
+        #redondeamos a 4 decimales
+        i2 = round(i2, 4)
+        return JsonResponse({"result": i2})
+
     except ValueError:
         return JsonResponse({"error": "Invalid input. Please provide valid numbers."})
